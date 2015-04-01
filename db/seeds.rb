0264100d7d0f57f7258f1a9c8ed2f2235dd15375
Partho Ghosh <partho@redcarpetup.com>
2015-04-01 23:33:08 +0530

    Seed file updated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+if !ShowcaseCap.exists?(1)
+	ShowcaseCap.create!(quota:2500, end_time:Time.now.to_datetime+7.days, start_time:Time.now.to_datetime+7.days+30.minutes)	
+end
