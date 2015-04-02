748fd475aed134f8d7dc17642ef582e81aed3402
Partho Ghosh <partho@redcarpetup.com>
2015-04-02 23:02:25 +0530

    Puma updated

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,5 @@
 threads 8,32
 workers 2
-bind  "unix:///opt/run/candeo_api.sock"
-pidfile "tmp/pids/puma.pid"
-state_path "tmp/pids/puma.state"
 on_worker_boot do
 	ActiveSupport.on_load(:active_record) do
     	ActiveRecord::Base.establish_connection
