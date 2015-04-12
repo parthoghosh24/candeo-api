5348c1c9299e27a324456dda54d4b4249a3d0ba6
Partho Ghosh <defjam24@gmail.com>
2015-04-12 23:15:15 +0530

    Sender email updated

--- app/mailers/candeo_mailer.rb
+++ app/mailers/candeo_mailer.rb
@@ -1,9 +1,9 @@
 class CandeoMailer < ActionMailer::Base
-  default from: "gamealoon@gmail.com" #For testing... in prod this will change to no-reply@candeoapp.com
+  default from: "\"Candeo\"<no-reply@candeoapp.com>" #For testing... in prod this will change to no-reply@candeoapp.com
 
-  def verify_user(user, url)  	  	
+  def verify_user(user, url)
   	@user =user
   	@url=url
   	mail(to:user.email, subject:"Welcome To Candeo")
-  end	
+  end
 end
