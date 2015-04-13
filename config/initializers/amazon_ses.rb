80cbd8f1ce05f520a0300e6797daee39c86cec28
Partho Ghosh <partho@redcarpetup.com>
2015-04-13 21:57:45 +0530

    Amazon ses config updated

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  server: 'email-smtp.us-east-1.amazonaws.com',
-  access_key_id: SES_CONF['api_key'],
-  secret_access_key: SES_CONF['api_secret']
+  :server=> 'email-smtp.us-east-1.amazonaws.com',
+  :access_key_id=> SES_CONF['api_key'],
+  :secret_access_key=> SES_CONF['api_secret']
