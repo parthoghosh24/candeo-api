ca3d91c8f6e60c7a0530e667d4ae500d7cdbe138
Partho Ghosh <defjam24@gmail.com>
2015-04-28 23:44:07 +0530

    Some bug resolved in performances fetch

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -59,9 +59,9 @@ class Performance < ActiveRecord::Base
    def self.performance_to_hash(performance, has_user)
         performance_hash={}
         performance_hash=performance.as_json
-        bg_url = performance.showcase.content.content_media_map.media_map.media.media_type == 3 ? nil : performance.showcase.user.user_media_map.media_map.media_url
+        bg_url = performance.showcase.content.content_media_map && performance.showcase.content.content_media_map.media_map.media.media_type == 3 ? nil : performance.showcase.user.user_media_map.media_map.media_url
         performance_hash[:bg_url]=bg_url
-        performance_hash[:media_url]=performance.showcase.content.content_media_map.media_map.media_url
+        performance_hash[:media_url]=performance.showcase.content.content_media_map.media_map.media_url if performance.showcase.content.content_media_map
         if has_user
             performance_hash[:name]=performance.showcase.user.name
             performance_hash[:user_avatar_url]=performance.showcase.user.user_media_map.media_map.media_url
