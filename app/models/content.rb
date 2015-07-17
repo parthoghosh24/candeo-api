2a78463879b6f0f3bc780868ca486c0810c00e39
Partho Ghosh <defjam24@gmail.com>
2015-07-17 16:50:56 +0530

    Content API updated

--- app/models/content.rb
+++ app/models/content.rb
@@ -54,11 +54,12 @@ class Content < ActiveRecord::Base
 
     if content
         contentHash = content.as_json
+        id=content.id
         bg_url = content.content_media_map && content.content_media_map.media_map.media.media_type == 3 ? nil : content.user.user_media_map.media_map.media_url
         if(type == 1)
-          contentHash[:has_been_inspired]=ResponseMap.exists?(user_id:params[:user_id], showcase_id:params[:id], is_inspired:1)
+          contentHash[:has_been_inspired]=ResponseMap.exists?(user_id:params[:user_id], showcase_id:id, is_inspired:1)
         else
-          contentHash[:has_been_inspired]=ResponseMap.exists?(user_id:params[:user_id], status_id:params[:id], is_inspired:1)
+          contentHash[:has_been_inspired]=ResponseMap.exists?(user_id:params[:user_id], status_id:id, is_inspired:1)
         end
         puts "bg url #{bg_url}"
         contentHash[:media_type]=content.content_media_map ? content.content_media_map.media_map.media.media_type : 0
@@ -70,11 +71,11 @@ class Content < ActiveRecord::Base
         contentHash[:user_avatar_url]=content.user.user_media_map.media_map.media_url
         if type == 1
            contentHash[:title]=content.shareable.title
-           contentHash[:appreciate_count]=ResponseMap.where(showcase_id:params[:id], has_appreciated:true).size()
-           contentHash[:skip_count]=ResponseMap.where(showcase_id:params[:id], has_skipped:true).size()
-           contentHash[:inspired_count]=ResponseMap.where(showcase_id:params[:id], is_inspired:true).size()
+           contentHash[:appreciate_count]=ResponseMap.where(showcase_id:id, has_appreciated:true).size()
+           contentHash[:skip_count]=ResponseMap.where(showcase_id:id, has_skipped:true).size()
+           contentHash[:inspired_count]=ResponseMap.where(showcase_id:id, is_inspired:true).size()
         else
-           contentHash[:inspired_count]=ResponseMap.where(status_id:params[:id], is_inspired:true).size()
+           contentHash[:inspired_count]=ResponseMap.where(status_id:id, is_inspired:true).size()
         end
         contentHash[:created_at]=content.created_at.strftime("%d %B, %Y")
 
